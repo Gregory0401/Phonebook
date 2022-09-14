fbdfcb953835f9f5886af59529fbaacbfd896d76
Gregory0401 <grigoriosdart@gmail.com>
2022-09-14 21:14:13 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,42 +7,53 @@ import { nanoid } from 'nanoid'
 import {useState, useEffect} from 'react';
 
  export default function App() {
-
-const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')));
-
+    const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) || []);
     const [filter, setFilter] = useState('');
    
-      useEffect(() => {
-        const ourContacts = localStorage.getItem('contacts');
-        if(ourContacts) setContacts(JSON.parse(ourContacts));
-       
-      }, []);
+    useEffect(() => {
+      const ourContacts = localStorage.getItem('contacts');
+      if(ourContacts) {
+        setContacts(JSON.parse(ourContacts))
+      };
+     
+    }, []);
       
-      useEffect(() => {    
-          localStorage.setItem('contacts', JSON.stringify(contacts))        
-      }, [contacts]);
+    useEffect(() => {    
+        localStorage.setItem('contacts', JSON.stringify(contacts))        
+    }, [contacts]);
 
 
-  const onSubmit = (name, number) => {
-    if (contacts.find(contact => contact.name === name)) {
-      return alert(`${name} is already in contacts.`);
+    const onSubmit = (name, number) => {
+      if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+        return alert(`${name} is already in contacts.`);
     }
-    
-  setContacts(prevState => prevState.slice(0).concat({id: nanoid(), name, number}))
+    setContacts(prevState => [...prevState, {id: nanoid(), name, number}])
+  // setContacts(prevState => prevState.slice(0).concat({id: nanoid(), name, number}))
 };
  
 
 const handleRemoveContact = id => 
 setContacts(prevState => prevState.filter(contact => contact.id !== id))
 
-const handleFilterChange = filter => setFilter({filter});
+
+
+const handleFilterChange = e => {
+  setFilter(e.currentTarget.value);
+};
+
+// const getVisibleContacts = (contacts, filter) => {
+//   return contacts.filter(({name}) =>
+//     name.toLowerCase().includes(filter.toLowerCase()))
+// }
 
 const getVisibleContacts = () => {
   return contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase()))
 }
 
-const visibleContacts = getVisibleContacts();
+
+
+// const visibleContacts = getVisibleContacts();
 
       return(
         <>
@@ -53,8 +64,8 @@ const visibleContacts = getVisibleContacts();
         <div className={s.container}>
        
         <h2 className={s.header_contact}>Contacts</h2>
-        <Filter filter={filter} onChange={handleFilterChange}/>
-        <ContactList onRemove={handleRemoveContact}  contacts={visibleContacts}/>
+        <Filter  onChange={handleFilterChange}/>
+        <ContactList onRemove={handleRemoveContact}  contacts={getVisibleContacts()}/>
         </div>          
         </>
       )
